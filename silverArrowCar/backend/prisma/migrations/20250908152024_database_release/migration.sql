c0ea7c44dfb6eb63f0e217488af10d08db275070
horvathmartin97 <horvathmartinn@icloud.com>
2025-09-08 17:30:13 +0200

    feat: initialize backend with Prisma, Express, and TypeScript
    - Added package.json for backend dependencies and scripts
    - Created Prisma migration files for User, Car, and Garage tables
    - Defined Prisma schema with User, Car, and Garage models
    - Added TypeScript configuration for the backend
    - Created initial index.ts file for server entry point

---
+++ silverArrowCar/backend/prisma/migrations/20250908152024_database_release/migration.sql
@@ -0,0 +1,49 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "profileImage" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Car" (
+    "id" TEXT NOT NULL,
+    "carModel" TEXT NOT NULL,
+    "carBrand" TEXT NOT NULL,
+    "builtYear" INTEGER NOT NULL,
+    "description" TEXT,
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdById" TEXT NOT NULL,
+
+    CONSTRAINT "Car_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Garage" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "carModel" TEXT NOT NULL,
+    "carBrand" TEXT NOT NULL,
+    "builtYear" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Garage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."Car" ADD CONSTRAINT "Car_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Garage" ADD CONSTRAINT "Garage_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
