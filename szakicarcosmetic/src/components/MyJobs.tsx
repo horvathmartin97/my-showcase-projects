bebc8429709b7f441129285baeaa97f327386c47
horvathmartin97 <horvathmartinn@icloud.com>
2025-12-22 10:47:47 +0100

    fix

--- szakicarcosmetic/src/components/MyJobs.tsx
+++ szakicarcosmetic/src/components/MyJobs.tsx
@@ -34,7 +34,7 @@ import toyotaYarisBack2 from "../assets/toyotaYaris/toyotaYarisBack2.jpeg";
 import toyotaYarisInterior1 from "../assets/toyotaYaris/toyotaYarisInterior1.jpeg";
 import toyotaYarisInterior2 from "../assets/toyotaYaris/toyotaYarisInterior2.jpeg";
 import toyotaYarisBackDoor from "../assets/toyotaYaris/toyotaYarisBackDoor.jpeg";
-import glcBack from "../assets/mercedesGlc/mercedesGlcBack.jpeg";
+import glcBack from "../assets/mercedesGlc/mercedesGlc-Back.jpeg";
 import glcFront from "../assets/mercedesGlc/mercedesGlcFront.jpeg";
 import glcFront2 from "../assets/mercedesGlc/mercedesGlcFront2.jpeg";
 import glcTrunk from "../assets/mercedesGlc/mercedesGlcTrunk.jpeg";
