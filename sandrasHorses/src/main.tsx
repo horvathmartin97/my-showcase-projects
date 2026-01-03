a8d42764899b44602a288935558c0203578c763e
horvathmartin97 <horvathmartinn@icloud.com>
2026-01-03 15:11:18 +0100

    feat: add AboutMe component to display personal information

--- sandrasHorses/src/main.tsx
+++ sandrasHorses/src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Header />
-    <main></main>
+    <main>
+      <AboutMe />
+    </main>
   </StrictMode>
 );
