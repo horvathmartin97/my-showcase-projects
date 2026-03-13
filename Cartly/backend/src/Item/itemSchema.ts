ca6ab25655c12a61d4afc40ff7b529cab092db70
horvathmartin97 <horvathmartinn@icloud.com>
2026-03-13 11:37:23 +0100

    feat: add item schema validation using Zod

---
+++ Cartly/backend/src/Item/itemSchema.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const itemSchema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters" }),
+  quantity: z.number().min(1, { message: "Quantity must be at least 1" }),
+});
+export type ItemSchemaType = z.infer<typeof itemSchema>;
