f92344af91754723be4c37f97b6f5b6cf154cbe3
horvathmartin97 <horvathmartinn@icloud.com>
2026-03-13 13:28:22 +0100

    feat: implement deleteItem method in itemService with error handling for non-existent items

--- Cartly/backend/src/Item/itemService.ts
+++ Cartly/backend/src/Item/itemService.ts
@@ -1,4 +1,5 @@
 import { Item } from "../../generated/prisma";
+import HttpError from "../utils/HttpError";
 import prisma from "../utils/prisma";
 import { ItemSchemaType } from "./itemSchema";
 
@@ -9,4 +10,14 @@ const itemService = {
     });
     return newItem;
   },
+  deleteItem: async (itemId: string): Promise<Item> => {
+    const isItemExist = await prisma.item.findUnique({
+      where: { id: itemId },
+    });
+    if (!isItemExist) {
+      throw new HttpError("Item is not found", 404);
+    }
+    return await prisma.item.delete({ where: { id: itemId } });
+  },
 };
+export default itemService;
