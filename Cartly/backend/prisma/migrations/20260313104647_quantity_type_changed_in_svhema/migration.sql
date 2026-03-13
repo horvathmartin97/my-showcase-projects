a8504c0c41469c548393e21042d153745caf9d43
horvathmartin97 <horvathmartinn@icloud.com>
2026-03-13 11:47:19 +0100

    feat: add migration to change quantity type in Item table from string to integer

---
+++ Cartly/backend/prisma/migrations/20260313104647_quantity_type_changed_in_svhema/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `quantity` to the `Item` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Item" DROP COLUMN "quantity",
+ADD COLUMN     "quantity" INTEGER NOT NULL;
