d27be842c85f3a31b8cdab853604ffe3b11728eb
horvathmartin97 <horvathmartinn@icloud.com>
2026-03-17 14:44:01 +0100

    feat: implement initial structure for LoginPage with context and state management

--- Cartly/frontend/src/pages/loginPage.tsx
+++ Cartly/frontend/src/pages/loginPage.tsx
@@ -1,3 +1,13 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { useNavigate } from "react-router";
+
 export default function LoginPage() {
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
   return <div></div>;
 }
