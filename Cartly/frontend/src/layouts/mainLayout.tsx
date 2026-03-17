4b7a42ef87bd7a621a07653719119f7c4d78febe
horvathmartin97 <horvathmartinn@icloud.com>
2026-03-17 14:07:24 +0100

    feat: implement authentication context and services with JWT handling

---
+++ Cartly/frontend/src/layouts/mainLayout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router";
+
+export default function MainLayout() {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-700">
+      <main className="px-2 md:px-40 py-4 ">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
--- Cartly/frontend/src/main.tsx
+++ Cartly/frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import MainLayout from "./layouts/mainLayout";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<MainLayout />}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
